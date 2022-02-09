876030c4cacc5c4a4d0ff2fb60329fcc65f512ab
Nikolai <nikolai.selivanov@productmadness.com>
2022-02-09 19:06:54 +0300

    Small fix

--- LiveCapture/RuntimeBridge/PlayableDirectorInternal.cs
+++ LiveCapture/RuntimeBridge/PlayableDirectorInternal.cs
@@ -8,7 +8,7 @@ namespace Unity.LiveCapture.Internal
     {
         public static void ResetFrameTiming()
         {
-            // awersome unity devs thing that it is a great idea to let access for internal methods to their modules
+            // awersome unity devs think that it is a great idea to let access for internal methods to their modules
             // so when I migrate it to separate nuget dll i have to use a reflection to call this.
             var playableDirectorType = typeof(PlayableDirector);
             MethodInfo staticMethod = playableDirectorType.GetMethod("ResetFrameTiming", BindingFlags.Static | BindingFlags.Public);
